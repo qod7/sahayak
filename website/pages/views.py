4853f4001414d89518b63cf5a279fa1c9e0fe07b
diwasblack <diwasblack@gmail.com>
2014-12-22 00:11:09 +0545

    Login page

--- website/pages/views.py
+++ website/pages/views.py
@@ -14,7 +14,7 @@ def login(request):
         user=auth.authenticate(username=username,password=password)
         if user is not None:
             auth.login(request,user)
-            return redirect('categories')
+            return redirect('categorylist')
         else:
             return render(request,'login.html',{'message':'Invalid login'})
     else:
