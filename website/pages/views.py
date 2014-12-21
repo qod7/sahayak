ee623eb2843e364f8a95cc508831c791d5db2723
Anup pokhrel <virtualanup@gmail.com>
2014-12-21 17:08:37 +0545

    Added base

--- website/pages/views.py
+++ website/pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template import RequestContext, loader
+from django.http import HttpResponse,HttpRequest,HttpResponseRedirect
+from django.shortcuts import render,redirect,get_object_or_404
 
-# Create your views here.
+def home(request):
+    template = loader.get_template('base.html')
+    context = RequestContext(request, {
+        'title': "Sahayak",
+        'mainmenuindex': 1,
+    })
+
+    return HttpResponse(template.render(context))
--- website/sahayak/urls.py
+++ website/sahayak/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sahayak.views.home', name='home'),
+    url(r'^$', 'pages.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
